6773f26f1c7d67069e2b9e18c816f33f8db081d2
ToSaveData <errandt@gmail.com>
2022-12-13 19:21:49 +0900

    opencv vector, event, etc

--- bmptask/bmpinfo.c
+++ bmptask/bmpinfo.c
@@ -16,40 +16,39 @@ int main(int argc, char **argv)
 	BITMAPFILEHEADER bmpFileHeader;
 	BITMAPINFOHEADER bmpInfoHeader;
 	RGBQUAD pal;
-
+	
 	if(argc != 2){
-		printf("Usage : ./%s xxx.bmp\n", argv[0]);
+		printf("Usage : ./ %s xxx.bmp \n", argv[0]);
 		return -1;
 		}
-
-	fp = fopen(argv[1], "rb");
-
-	if(fp == NULL){
+	if((fp = fopen(argv[1], "rb")) == NULL){
 		perror("ERROR\n");
 		return -1;
 		}
-
-	fread(&bmpFileHeader, sizeof(BITMAPFILEHEADER), 1, fp);
-	fread(&bmpInfoHeader, sizeof(BITMAPINFOHEADER), 1, fp);
-	//fread(&pal, sizeof(RGBQUAD), 1, fp);
-
-	elemsize = bmpInfoHeader.biBitCount/8;
 	pos = (ubyte*)malloc(sizeof(ubyte*) * bmpInfoHeader.biHeight * bmpInfoHeader.biWidth * elemsize);
 	inimg = (ubyte*)malloc(sizeof(ubyte*) * bmpInfoHeader.biHeight * bmpInfoHeader.biWidth * elemsize);
+	fread(&bmpFileHeader, sizeof(BITMAPFILEHEADER), 1, fp);
+	fread(&bmpInfoHeader, sizeof(BITMAPINFOHEADER), 1, fp);
+	fread(&inimg, sizeof(ubyte) * bmpInfoHeader.SizeImage, 1, fp);
+
+	elemsize = bmpInfoHeader.biBitCount/8;
 	width = bmpInfoHeader.biWidth*elemsize;
 
 	for(int i = 0; i < bmpInfoHeader.biHeight -1; i++){
-		for(int j = 0; j < bmpInfoHeader.biWidth*3; j++){
+		for(int j = 0; j < bmpInfoHeader.biWidth*elemsize; j++){
 			for(int k = 0; k < elemsize; k++){
-				printf("(%s, %s, %s)\n", pos[, g, b);
+				pos[i*width + j*bmpInfoHeader.biHeight + k] = inimg[(bmpInfoHeader.biHeight-i)*width -((bmpInfoHeader.biWidth-j-1)*elemsize) + k];
+				//printf("(%s ", pos[i*width + j*bmpInfoHeader.biHeight + k]);
+				return 0;
 				}
+				printf("\n");
+				return 0;
 			}
 		}
-				
-	printf("(%s, %s, %s)\n", r, g, b);
 	
-	fclose(fp);
 	free(fp);
+	free(inimg);
 
+	fclose(fp);
 	return 0;
 }
